c146ca563df86f7940c825e1679bbea010e35b1b
Tatiana Andreeva <snurnisyan@gmail.com>
2026-05-18 20:32:31 +0200

    feat: add layput for pet delete

--- frontend/components/pets/tabs/OverviewTab.tsx
+++ frontend/components/pets/tabs/OverviewTab.tsx
@@ -1,4 +1,5 @@
-import { Grid, GridItem, Stack } from "@chakra-ui/react";
+import { Flex, Grid, GridItem, Stack } from "@chakra-ui/react";
+import { DeletePetButton } from "@/components/pets/DeletePetButton";
 import { NotesSection } from "@/components/pets/overview/NotesSection";
 import { OverviewSection } from "@/components/pets/overview/OverviewSection";
 import { VaccinesSection } from "@/components/pets/overview/VaccinesSection";
@@ -12,20 +13,26 @@ type TOverviewTabProps = {
 
 export function OverviewTab({ pet, backendPetId }: TOverviewTabProps) {
   return (
-    <Grid templateColumns={["1fr", null, null, "2fr 1fr"]} gap="20px">
-      <GridItem>
-        <Stack gap="20px">
-          <OverviewSection pet={pet} backendPetId={backendPetId} />
-          <VaccinesSection backendPetId={backendPetId} />
-        </Stack>
-      </GridItem>
+    <Stack gap="20px">
+      <Grid templateColumns={["1fr", null, null, "2fr 1fr"]} gap="20px">
+        <GridItem>
+          <Stack gap="20px">
+            <OverviewSection pet={pet} backendPetId={backendPetId} />
+            <VaccinesSection backendPetId={backendPetId} />
+          </Stack>
+        </GridItem>
 
-      <GridItem>
-        <Stack gap="20px">
-          <NotesSection notes={pet.notes} backendPetId={backendPetId} />
-          <VetSection vet={pet.vet} backendPetId={backendPetId} />
-        </Stack>
-      </GridItem>
-    </Grid>
+        <GridItem>
+          <Stack gap="20px">
+            <NotesSection notes={pet.notes} backendPetId={backendPetId} />
+            <VetSection vet={pet.vet} backendPetId={backendPetId} />
+          </Stack>
+        </GridItem>
+      </Grid>
+
+      <Flex justify="center">
+        <DeletePetButton petName={pet.name} />
+      </Flex>
+    </Stack>
   );
 }
